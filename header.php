a812142e4d3fedeaa1ec5cd5ff343ab521960b77
ry1015 <ry1015@gmail.com>
2014-10-18 16:29:13 -0700

    Added Index and Header pages

---
+++ header.php
@@ -0,0 +1,11 @@
+<?php
+echo <<<_END
+    <html>
+        <head>
+            <title>F8L Online Banking</title>
+            <link rel='stylesheet' type='text/css' href='style.css'>
+        </head>
+        <body>
+            
+_END;
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'header.php';
+
+
+echo <<<_END
+    </body>
+    </html>
+_END;
+?>
