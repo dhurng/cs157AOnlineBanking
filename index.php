0e3386dacfed04cc3f90f7a2813233a3784b9fd2
dhurng <dhurng@gmail.com>
2014-11-06 21:50:19 -0800

    Update index.php
    just added a few simple decorations that we can fill in later, some buttons for functions that will be in php

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ echo <<<_END
          }
          coming_soon();
    </script>
-
     <!--drop down navigation bar -->
       <!--===================================================================-->
       <div class="navbar navbar-inverse navbar-fixed-top"> <!--navbar-inverse for black bar/default for white-->
@@ -24,34 +23,64 @@ echo <<<_END
            		</button>
              <div class="navbar-brand"><i class="fa fa-bank"></i> F8L Exception Online Banking System</div>
          </div>
-
          <!--navbar collapse content-->
       	 <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
             	<li class="active"><a href="#">Home</a></li>
-
             	<!--maybe write this part in php but for now-->
             	<li><a href="#">Login</a></li>
-
-
            		<li><a href="#">About</a></li> 
            	</ul>
       	 </div>
       </div>
-
+      </div>
       <!--Jumbotron, huge box in the front for promotions-->
       <!--============================================================-->
-
       <div class="jumbotron">
-         <h1>Welcome to Bank?</h1>
+         <h1>Welcome to F8L Exception Bank</h1>
          <p>
-            Hello Again
+            <a href="#" class="btn btn-info btn-lg">Quickdraw</a>
+            <a href="#" class="btn btn-info btn-lg">View Balance</a>
+            <a href="#" class="btn btn-info btn-lg">Help</a>
+            <a href="#" class="btn btn-info btn-lg">Loans</a>
          </p>
+      </div>
+
+      <div class="container">
+		 <div class="row">
+    		<div class="col-md-3">
+    			<h3>Enroll Today!</h3>
+      			<p>
+      				enrollment info here
+      			</p>
+   			</div>
+
+   		 	<div class="col-md-3"> 
+   		 		<h3>Earn Cache Rewards</h3>
+      			<p>
+      				info
+      			</p>
+    		</div>
+
+    		<div class="col-md-3"> 
+    			<h3>Need help with Home Loan Payments?</h3>
+     			<p>
+     		 		info
+     		 	</p>
+    		</div>
+
+    		<div class="col-md-3"> 
+    			<h3>Privacy & Security</h3>
+      			<p>
+      				info
+      			</p>
+    		</div>
+ 		</div>
       </div>
 
       <!--fixated footer-->
       <!--=============================================================-->
-      <div class="navbar navbar-default navbar-fixed-bottom">
+      <div class="navbar navbar-inverse navbar-fixed-bottom">
          <div class="container">
             <p class="navbar-text pull-left"><i class="fa fa-cc-paypal"></i> <i class="fa fa-cc-amex"></i> <i class="fa fa-cc-discover"></i> <i class="fa fa-cc-mastercard"></i> <i class="fa fa-cc-visa"></i></p>
          </div>
