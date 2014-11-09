57b2970971b6389a1b6a56f6371e3849ada93cd7
dhurng <dhurng@gmail.com>
2014-11-08 16:34:19 -0800

    Update index.php
    Updated front end page, need php for login and functions

--- index.php
+++ index.php
@@ -2,17 +2,23 @@
     require_once 'header.php';
     
 echo <<<_END
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	  <!-- Bootstrap will be in header but for now use for testing due to functions.php db issues-->
+	  <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="css/styles.css">
+      <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+</head>
 <body>
-  <script type="text/javascript">
-  //site is still in works
-         function coming_soon() {
-            alert("Site is still in works");
-         }
-         coming_soon();
-   </script>
     <!--drop down navigation bar -->
       <!--===================================================================-->
-      <div class="navbar navbar-inverse navbar-fixed-top"> <!--navbar-inverse for black bar/default for white-->
+      <div class="navbar navbar-inverse"> <!--navbar-inverse for black bar/default for white-->
          <div class="container">
         	 <div class="navbar-header">
         		 <!--this is so that when the window collapses it will show a button list-->
@@ -26,11 +32,11 @@ echo <<<_END
          </div>
          <!--navbar collapse content-->
       	 <div class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
+            <ul class="nav nav-pills navbar-right" role="tablist">
             	<li class="active"><a href="#">Home</a></li>
             	<!--maybe write this part in php but for now-->
             	<li><a href="#">Login</a></li>
-           		<li><a href="#">About</a></li> 
+           		<li><a href="about.html">About</a></li> 
            	</ul>
       	 </div>
       </div>
@@ -38,7 +44,7 @@ echo <<<_END
       <!--Jumbotron, huge box in the front for promotions-->
       <!--============================================================-->
       <div class="jumbotron">
-         <h1>Welcome to F8L Exception Bank</h1>
+         <h1>Online Banking with F8L Exception</h1>
          <p>
             <a href="#" class="btn btn-info btn-lg">Quickdraw</a>
             <a href="#" class="btn btn-info btn-lg">View Balance</a>
@@ -52,28 +58,30 @@ echo <<<_END
     		<div class="col-md-3">
     			<h3>Enroll Today!</h3>
       			<p>
-      				enrollment info here
+      				With Online Banking you can check your balance and account activity or search your statements and payment history whenever you like from your laptop, tablet or smartphone. You can also create email and text alerts Footnote 1 that let you know if your account balance is low or if a payment is due to help you avoid fees.
       			</p>
    			</div>
 
    		 	<div class="col-md-3"> 
    		 		<h3>Earn Cache Rewards</h3>
       			<p>
-      				info
+      				Online-exclusive $100 cash rewards bonus after qualifying purchase(s)
+Earn more cash back for the things you buy most
       			</p>
     		</div>
 
     		<div class="col-md-3"> 
     			<h3>Need help with Home Loan Payments?</h3>
      			<p>
-     		 		info
+     		 		If you're a homeowner struggling with your loan payments, you may want to learn about our home loan assistance programs. Bank of America is committed to helping homeowners and is a participant in the national mortgage settlement agreement.
      		 	</p>
     		</div>
 
     		<div class="col-md-3"> 
     			<h3>Privacy & Security</h3>
       			<p>
-      				info
+      				Comprehensive protection. Confident banking.
+We're committed to keeping your personal and financial information safe and secure
       			</p>
     		</div>
  		</div>
@@ -83,7 +91,7 @@ echo <<<_END
       <!--=============================================================-->
       <div class="navbar navbar-inverse navbar-fixed-bottom">
          <div class="container">
-            <p class="navbar-text pull-left"><i class="fa fa-cc-paypal"></i> <i class="fa fa-cc-amex"></i> <i class="fa fa-cc-discover"></i> <i class="fa fa-cc-mastercard"></i> <i class="fa fa-cc-visa"></i></p>
+            <p class="navbar-text pull-right"><i class="fa fa-cc-paypal"></i> <i class="fa fa-cc-amex"></i> <i class="fa fa-cc-discover"></i> <i class="fa fa-cc-mastercard"></i> <i class="fa fa-cc-visa"></i></p>
          </div>
       </div>
 
@@ -94,5 +102,6 @@ echo <<<_END
       <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
+
 _END;
 ?>
